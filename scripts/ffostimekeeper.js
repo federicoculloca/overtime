ea223d2183fe62ca2cc8f37b49521d7e00a453a0
Federico Culloca <djthrottle@gmail.com>
2013-11-01 13:09:24 +0100

    basic structure, no style, no saving, basic list filling

---
+++ scripts/ffostimekeeper.js
@@ -0,0 +1,50 @@
+var taskList = [];
+
+function TKTask(title, hours, minutes) {
+    this.title = title;
+    this.hours = hours;
+    this.minutes = minutes;
+    this.uid = generateUID();
+}
+
+function generateUID(){
+    var d = new Date().getTime();
+    var uid = 'txxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = (d + Math.random()*16)%16 | 0;
+        d = Math.floor(d/16);
+        return (c=='x' ? r : (r&0x7|0x8)).toString(16);
+    });
+    return uid;
+};
+
+function zeroPad(n, p, c) {
+    var pad_char = typeof c !== 'undefined' ? c : '0';
+    var pad = new Array(1 + p).join(pad_char);
+    return (pad + n).slice(-pad.length);
+}
+
+function redrawTaskList() {
+    $('#taskList ul').empty();
+    for (var i = taskList.length; i-->0; ) {
+        var currentTask = taskList[i];
+        var task = "<li><span>" + currentTask.title + "</span><span>" + zeroPad(currentTask.hours, 2) + ":" + zeroPad(currentTask.minutes, 2) + "</span></li>";
+        $('#taskList ul').append(task);
+    }
+}
+
+$(document).ready( function () {
+    $('#addTaskOk').click(function () {
+        var title = $('#addTaskTitle').val();
+        var hours = parseInt($('#addTaskDurationH').val(), 10) || 0;
+        var minutes = parseInt($('#addTaskDurationM').val(), 10) || 0;
+        var newTask = new TKTask(title, hours, minutes);
+        taskList.push(newTask);
+        $('#addTaskTitle').val("");
+        $('#addTaskDurationH').val("");
+        $('#addTaskDurationM').val("");
+        redrawTaskList();
+        return true;
+    });
+});
+
+
